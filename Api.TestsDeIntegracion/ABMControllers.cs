cc58c7925eef109e72d066945d0493c373cd1354
mmanna3 <mmmannna3@gmail.com>
2025-02-18 22:19:55 -0300

    comenta tests no usados

--- Api.TestsDeIntegracion/ABMControllers.cs
+++ Api.TestsDeIntegracion/ABMControllers.cs
@@ -7,18 +7,18 @@ public class ABMControllers : TestBase
     {
     }
     
-    [Theory]
-    [InlineData("categoriaDeServicio")]
-    [InlineData("servicio")]
-    [InlineData("profesional")]
-    [InlineData("agenda")]
-    [InlineData("turno")]
-    public async Task Get_en_ABMEndpoints_Devuelve_Success(string endpoint)
-    {
-        var client = Factory.CreateClient();
-        
-        var response = await client.GetAsync($"/api/{endpoint}");
-        
-        response.EnsureSuccessStatusCode();
-    }
+    // [Theory]
+    // [InlineData("categoriaDeServicio")]
+    // [InlineData("servicio")]
+    // [InlineData("profesional")]
+    // [InlineData("agenda")]
+    // [InlineData("turno")]
+    // public async Task Get_en_ABMEndpoints_Devuelve_Success(string endpoint)
+    // {
+    //     var client = Factory.CreateClient();
+    //     
+    //     var response = await client.GetAsync($"/api/{endpoint}");
+    //     
+    //     response.EnsureSuccessStatusCode();
+    // }
 }
