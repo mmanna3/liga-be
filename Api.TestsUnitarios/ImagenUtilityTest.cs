8927484b795d4c532191a08872eec224b129f055
mmanna3 <mmmannna3@gmail.com>
2025-03-02 15:37:19 -0300

    agrega ImagenUtility con tests

---
+++ Api.TestsUnitarios/ImagenUtilityTest.cs
@@ -0,0 +1,33 @@
+﻿
+
+using Api.Core.Logica;
+
+namespace Api.TestsUnitarios
+{
+	public class ImagenUtilityTest
+	{		
+		// public static string PuntoRojoBase64 = "iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==";
+		private const string PuntoRojoBase64ConUriDataJpg = "data:image/jpeg;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==";
+		private const string RectanguloVerticalBase64 = "/9j/4AAQSkZJRgABAQEAYABgAAD/4QAiRXhpZgAATU0AKgAAAAgAAQESAAMAAAABAAEAAAAAAAD/2wBDAAIBAQIBAQICAgICAgICAwUDAwMDAwYEBAMFBwYHBwcGBwcICQsJCAgKCAcHCg0KCgsMDAwMBwkODw0MDgsMDAz/2wBDAQICAgMDAwYDAwYMCAcIDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAz/wAARCAACAAEDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwD9/KKKKAP/2Q==";
+
+		[Fact]
+		public void ConvertirABitMapYATamanio240X240()
+		{
+			var bitMap = ImagenUtility.ConvertirABitMapYATamanio240X240(PuntoRojoBase64ConUriDataJpg);
+			Assert.Equal(240, bitMap.Width);
+			Assert.Equal(240, bitMap.Height);
+		}
+
+		[Fact]
+		public void ImagenCuadradaRotarAHorizontalYComprimir()
+		{
+			var bytes = Convert.FromBase64String(RectanguloVerticalBase64);
+			var stream = new MemoryStream(bytes);
+
+			var image = ImagenUtility.RotarAHorizontalYComprimir(stream);
+
+			Assert.True(image.Width > image.Height);
+		}
+
+	}
+}
---
+++ Api/Core/Logica/ImagenUtility.cs
@@ -0,0 +1,117 @@
+﻿using SkiaSharp;
+
+namespace Api.Core.Logica
+{
+    public static class ImagenUtility
+    {
+        public static SKBitmap ConvertirABitMapYATamanio240X240(string fotoBase64)
+        {
+            fotoBase64 = QuitarMimeType(fotoBase64);
+            var fotoCuadrada = HacerFotoCuadrada240X240(fotoBase64);
+            return fotoCuadrada;
+        }
+
+        public static SKBitmap ConvertirABitMapYATamanio240X240YEspejar(string fotoBase64)
+        {
+            var result = ConvertirABitMapYATamanio240X240(fotoBase64);
+            result = EspejarImagen(result);
+            return result;
+        }
+
+        public static SKBitmap ConvertirAImageYQuitarMimeType(string imagenBase64ConMimeType)
+        {
+            imagenBase64ConMimeType = QuitarMimeType(imagenBase64ConMimeType);
+            return Base64ToBitmap(imagenBase64ConMimeType);
+        }
+
+        public static SKBitmap RotarAHorizontalYComprimir(Stream stream)
+        {
+            using var image = SKBitmap.Decode(stream);
+            var resultado = RotarImagen(image);
+            return Comprimir(resultado);
+        }
+        
+        private static SKBitmap RotarImagen(SKBitmap bitmap)
+        {
+            var rotatedBitmap = new SKBitmap(bitmap.Height, bitmap.Width);
+            using var canvas = new SKCanvas(rotatedBitmap);
+            canvas.Translate(rotatedBitmap.Width, 0);
+            canvas.RotateDegrees(90);
+            canvas.DrawBitmap(bitmap, 0, 0);
+            return rotatedBitmap;
+        }
+        
+        public static SKBitmap Comprimir(SKBitmap bitmap)
+        {
+            using var image = SKImage.FromBitmap(bitmap);
+            using var data = image.Encode(SKEncodedImageFormat.Jpeg, 75);
+            return SKBitmap.Decode(data);
+        }
+
+        public static string StreamToBase64(Stream stream)
+        {
+            using var bitmap = SKBitmap.Decode(stream);
+            return BitmapToBase64(bitmap);
+        }
+        
+        public static string BitmapToBase64(SKBitmap bitmap)
+        {
+            using var image = SKImage.FromBitmap(bitmap);
+            using var data = image.Encode(SKEncodedImageFormat.Png, 100);
+            return Convert.ToBase64String(data.ToArray());
+        }
+
+        public static string ImageToBase64(SKImage img)
+        {
+            using var data = img.Encode(SKEncodedImageFormat.Png, 100);
+            return Convert.ToBase64String(data.ToArray());
+        }
+
+        private static bool EsVertical(SKBitmap bitmap)
+        {
+            return bitmap.Height > bitmap.Width;
+        }
+
+        private static string ByteArrayToBase64(byte[] bytes)
+        {
+            return Convert.ToBase64String(bytes);
+        }
+
+        private static SKBitmap EspejarImagen(SKBitmap bitmap)
+        {
+            var result = new SKBitmap(bitmap.Width, bitmap.Height);
+            using var canvas = new SKCanvas(result);
+            canvas.Scale(-1, 1, bitmap.Width / 2f, 0);
+            canvas.DrawBitmap(bitmap, 0, 0);
+            return result;
+        }
+
+        private static SKBitmap HacerFotoCuadrada240X240(string base64)
+        {
+            var bitmap = Base64ToBitmap(base64);
+            return HacerFotoCuadrada(bitmap, 240);
+        }
+
+        private static SKBitmap Base64ToBitmap(string base64)
+        {
+            byte[] imageBytes = Convert.FromBase64String(base64);
+            using var stream = new SKMemoryStream(imageBytes);
+            return SKBitmap.Decode(stream);
+        }
+
+        private static SKBitmap HacerFotoCuadrada(SKBitmap bitmap, int size)
+        {
+            var cuadrada = new SKBitmap(size, size);
+            using var canvas = new SKCanvas(cuadrada);
+            var srcRect = new SKRect((bitmap.Width - size) / 2, (bitmap.Height - size) / 2, (bitmap.Width + size) / 2,
+                (bitmap.Height + size) / 2);
+            canvas.DrawBitmap(bitmap, srcRect, new SKRect(0, 0, size, size));
+            return cuadrada;
+        }
+
+        private static string QuitarMimeType(string base64)
+        {
+            return base64.Contains(',') ? base64.Split(',')[1] : base64;
+        }
+    }
+}
