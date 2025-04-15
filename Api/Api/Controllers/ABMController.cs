b5258bc871ad5d40254988927aa35f9b9a8ef1e3
mmanna3 <mmmannna3@gmail.com>
2025-04-15 01:04:38 -0300

    el rol consulta puede acceder a los get, pero no a post ni put

--- Api/Api/Controllers/ABMController.cs
+++ Api/Api/Controllers/ABMController.cs
@@ -8,7 +8,6 @@ namespace Api.Api.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-[Authorize(Roles = "Administrador")]
 public abstract class ABMController<TDTO, TCore> : ControllerBase
     where TDTO : DTO
     where TCore : ICoreABM<TDTO>
@@ -25,6 +24,7 @@ public abstract class ABMController<TDTO, TCore> : ControllerBase
     }
     
     [HttpGet]
+    [Authorize(Roles = "Administrador,Consulta")]
     public async Task<ActionResult<IEnumerable<TDTO>>> Get()
     {
         var dto = await Core.Listar();
@@ -32,6 +32,7 @@ public abstract class ABMController<TDTO, TCore> : ControllerBase
     }
     
     [HttpGet("{id}")]
+    [Authorize(Roles = "Administrador,Consulta")]
     public async Task<ActionResult<TDTO>> Get(int id)
     {
         var dto = await Core.ObtenerPorId(id);
@@ -41,6 +42,7 @@ public abstract class ABMController<TDTO, TCore> : ControllerBase
     
     // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
     [HttpPost]
+    [Authorize(Roles = "Administrador")]
     public virtual async Task<ActionResult<TDTO>> Crear(TDTO dto)
     {
         var id = await Core.Crear(dto);
@@ -52,6 +54,7 @@ public abstract class ABMController<TDTO, TCore> : ControllerBase
     // PUT: api/Servicio/5
     // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
     [HttpPut("{id}")]
+    [Authorize(Roles = "Administrador")]
     public async Task<IActionResult> Put(int id, TDTO dto)
     {
         if (id != dto.Id)
