e9d01456960317d8253bfe20cbe8b5a755514595
mmanna3 <mmmannna3@gmail.com>
2026-03-06 21:40:36 -0300

    arregla fichaje de delegados que son jugadores

--- Api/Persistencia/Repositorios/ImagenDelegadoRepo.cs
+++ Api/Persistencia/Repositorios/ImagenDelegadoRepo.cs
@@ -13,16 +13,39 @@ namespace Api.Persistencia.Repositorios
 
 		public void CopiarFotosDeDelegadoExistenteATemporales(string dni)
 		{
-			CopiarCarnetATemporales($"{ImagenesDefinitivasAbsolute}/{dni}.jpg", dni);
+			CopiarCarnetATemporales($"{ImagenesDefinitivasAbsolute}/{dni}.jpg", dni, origenEnTemporalesCarnet: false);
 		}
 
 		public void CopiarFotosDeJugadorATemporales(string dni)
 		{
-			var carnetJugador = $"{Paths.ImagenesJugadoresAbsolute}/{dni}.jpg";
-			CopiarCarnetATemporales(carnetJugador, dni);
+			var (pathOrigen, origenEnTemporalesCarnet) = ObtenerPathFotoJugador(dni);
+			CopiarCarnetATemporales(pathOrigen, dni, origenEnTemporalesCarnet);
 		}
 
-		private void CopiarCarnetATemporales(string pathOrigen, string dni)
+		private (string Path, bool OrigenEnTemporalesCarnet) ObtenerPathFotoJugador(string dni)
+		{
+			var pathJpg = System.IO.Path.Combine(Paths.ImagenesJugadoresAbsolute, $"{dni}.jpg");
+			if (File.Exists(pathJpg))
+				return (pathJpg, false);
+
+			if (Directory.Exists(Paths.ImagenesJugadoresAbsolute))
+			{
+				var archivosJugador = Directory.GetFiles(Paths.ImagenesJugadoresAbsolute, $"{dni}.*");
+				if (archivosJugador.Length > 0)
+					return (archivosJugador[0], false);
+			}
+
+			if (Directory.Exists(Paths.ImagenesTemporalesCarnetAbsolute))
+			{
+				var archivosTemporales = Directory.GetFiles(Paths.ImagenesTemporalesCarnetAbsolute, $"{dni}.*");
+				if (archivosTemporales.Length > 0)
+					return (archivosTemporales[0], true);
+			}
+
+			throw new ExcepcionControlada("No se encontraron fotos para el DNI indicado");
+		}
+
+		private void CopiarCarnetATemporales(string pathOrigen, string dni, bool origenEnTemporalesCarnet)
 		{
 			if (!File.Exists(pathOrigen))
 				throw new ExcepcionControlada("No se encontraron fotos para el DNI indicado");
@@ -31,9 +54,14 @@ namespace Api.Persistencia.Repositorios
 			Directory.CreateDirectory(Paths.ImagenesTemporalesDNIFrenteAbsolute);
 			Directory.CreateDirectory(Paths.ImagenesTemporalesDNIDorsoAbsolute);
 
-			File.Copy(pathOrigen, $"{Paths.ImagenesTemporalesCarnetAbsolute}/{dni}.jpg", overwrite: true);
-			File.Copy(pathOrigen, $"{Paths.ImagenesTemporalesDNIFrenteAbsolute}/{dni}.jpg", overwrite: true);
-			File.Copy(pathOrigen, $"{Paths.ImagenesTemporalesDNIDorsoAbsolute}/{dni}.jpg", overwrite: true);
+			var pathCarnet = $"{Paths.ImagenesTemporalesCarnetAbsolute}/{dni}.jpg";
+			var pathFrente = $"{Paths.ImagenesTemporalesDNIFrenteAbsolute}/{dni}.jpg";
+			var pathDorso = $"{Paths.ImagenesTemporalesDNIDorsoAbsolute}/{dni}.jpg";
+
+			if (!origenEnTemporalesCarnet)
+				File.Copy(pathOrigen, pathCarnet, overwrite: true);
+			File.Copy(pathOrigen, pathFrente, overwrite: true);
+			File.Copy(pathOrigen, pathDorso, overwrite: true);
 		}
 	}
 }
